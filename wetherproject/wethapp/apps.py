54fc8d0e0f6bca7cce792a56c9c3e74cb7a41433
Pranav.PL <pranavpl.tvm@gmail.com>
2024-08-14 14:45:19 +0530

    djwetheraapp first commet

---
+++ wetherproject/wethapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WethappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wethapp'
---
+++ wetherproject/wethapp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+# Create your views here.
+def home(request):
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'kochi'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={"e587aef25c37e3120e9db16978538444"}'
+    PARAMS = {'units':'metric'}
+
+    API_KEY =  'AIzaSyCMMV7sXOqbAc2UrOzybofZ4zHZae1ng1M'
+
+    SEARCH_ENGINE_ID = '13784aaa653044422'
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          # city = 'indore'
+          # data = requests.get(url,params=PARAMS).json()
+          
+          # description = data['weather'][0]['description']
+          # icon = data['weather'][0]['icon']
+          # temp = data['main']['temp']
+          day = datetime.date.today()
+
+
+    return render (request,'index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city})
